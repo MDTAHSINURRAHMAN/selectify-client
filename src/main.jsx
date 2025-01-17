7a78d202d31c7a32000ecf6a1fabb0255744c601
MDTAHSINURRAHMAN <mdtahsinurrahman1210@gmail.com>
2025-01-17 14:17:28 +0600

    Helmet Provider Added

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import router from './routes/router.jsx'
 import { RouterProvider } from 'react-router-dom'
 import { AuthProvider } from './providers/AuthProvider'
+// Helmet
 import { HelmetProvider } from 'react-helmet-async'
 
 createRoot(document.getElementById('root')).render(
